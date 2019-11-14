b14d5e57f640ac6c3cf186d031b0485c944624f7
Jack Toke <jackptoke@gmail.com>
2019-11-14 17:20:45 +1100

    Added modulo

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ const btnAdd = document.querySelector('.btnAdd');
 const btnSubtract = document.querySelector('.btnSubtract');
 const btnMultiply = document.querySelector('.btnMultiply');
 const btnDivide = document.querySelector('.btnDivide');
-
+const btnModulo = document.querySelector('.btnModulo');
 const numField1 = document.querySelector('.num1');
 const numField2 = document.querySelector('.num2');
+const lblResult = document.querySelector('.btnModulo');
 
 function add(){
 
@@ -26,7 +27,10 @@ function multiply(){
 function divide(){
 
 }
+
+
 btnAdd.addEventListener("click", add);
 btnSubtract.addEventListener("click", subtract);
 btnMultiply.addEventListener("click", multiply);
 btnDivide.addEventListener("click", divide);
+btnModulo.addEventListener("click", oddOrEven);
