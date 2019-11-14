c596be7c82f9baf398ed47aec98648fa6cc5ec59
Mark <markke4@outlook.com>
2019-11-14 17:07:07 +1100

    work

--- modulo.js
+++ modulo.js
@@ -1,9 +1,25 @@
+
 let oddOrEven = (num) => {
-  if(num % 2){
-    return true;
-  } else {
-    return false;
+  try {
+
+    if(typeof(num) !== "number"){
+      throw "Invalid input.";
+    } else if(num % 2){
+      return true;
+    } else {
+      return false;
+    }
+
+  } catch(error) {
+    return error;
   }
+
 }
 
-console.log("in the mark-branch");
+console.log(oddOrEven(3));
+console.log(oddOrEven("heyhey"));
+
+
+// console.log(typeof("sgfsg"))
+
+
