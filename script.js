e849f0559b01426d6021c1d5123bb9729b10ac66
Jack Toke <jackptoke@gmail.com>
2019-11-14 16:56:04 +1100

    Initial commit from Jack

---
+++ script.js
@@ -0,0 +1,32 @@
+//import Add script
+//import Subtract script
+//import multiply script
+//import divide script
+
+const btnAdd = document.querySelector('.btnAdd');
+const btnSubtract = document.querySelector('.btnSubtract');
+const btnMultiply = document.querySelector('.btnMultiply');
+const btnDivide = document.querySelector('.btnDivide');
+
+const numField1 = document.querySelector('.num1');
+const numField2 = document.querySelector('.num2');
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+btnAdd.addEventListener("click", add);
+btnSubtract.addEventListener("click", subtract);
+btnMultiply.addEventListener("click", multiply);
+btnDivide.addEventListener("click", divide);
