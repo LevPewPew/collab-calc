c8e8bb3a599c5774b3e83d82493774926e94e6a1
Mark <markke4@outlook.com>
2019-11-14 16:57:05 +1100

    ready to commit

--- class_merge.js
+++
@@ -1,7 +0,0 @@
-let moduloFunction = (num1, num2) => {
-  if(num1 % num2){
-
-  }
-}
-
-console.log(4, 2)
---
+++ modulo.js
@@ -0,0 +1,9 @@
+let oddOrEven = (num) => {
+  if(num % 2){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log("in the mark-branch");
